5bcbdaa8f6f8ccf85d0ee9fb218ff108e31e1452
HailGanyu <henryyanguk@gmail.com>
2022-10-28 22:34:03 +0100

    removed oestro g sphere

--- script.js
+++ script.js
@@ -79,12 +79,12 @@ let selectedObject = null;
         scene.add(sphereMesh4);
         sphereMesh4.position.set(-100, 130, 90);
 
-        const sphereGeometry5 = new THREE.SphereGeometry(30, 64, 32);
-        const sphereTex5 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/port3c/main/src/loadinglogo%20(1).png');
-        const sphereMaterial5 = new THREE.MeshStandardMaterial({ map: sphereTex5 });
-        const sphereMesh5 = new THREE.Mesh(sphereGeometry5, sphereMaterial5);
-        scene.add(sphereMesh5);
-        sphereMesh5.position.set(220, 125, -100);
+        // const sphereGeometry5 = new THREE.SphereGeometry(30, 64, 32);
+        // const sphereTex5 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/port3c/main/src/loadinglogo%20(1).png');
+        // const sphereMaterial5 = new THREE.MeshStandardMaterial({ map: sphereTex5 });
+        // const sphereMesh5 = new THREE.Mesh(sphereGeometry5, sphereMaterial5);
+        // scene.add(sphereMesh5);
+        // sphereMesh5.position.set(220, 125, -100);
 
         const sphereGeometry6 = new THREE.SphereGeometry(30, 64, 32);
         const sphereTex6 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/port3c/main/src/mesmo.png');
@@ -194,8 +194,8 @@ let selectedObject = null;
                 window.location.href = "https://ganyuhail.github.io/3dArt/";
             } else if (selectedObject === sphereMesh4) {
                 window.location.href = "https://www.instagram.com/hennohail/?hl=en";
-            } else if (selectedObject === sphereMesh5) {
-                window.location.href = "https://ganyuhail.github.io/enate/";
+            // } else if (selectedObject === sphereMesh5) {
+            //     window.location.href = "https://ganyuhail.github.io/enate/";
             } else if (selectedObject === sphereMesh6) {
                 window.location.href = "https://ganyuhail.github.io/mesmo1/";
             } else if (selectedObject === sphereMesh7) {
@@ -212,8 +212,8 @@ let selectedObject = null;
                 window.location.href = "https://ganyuhail.github.io/3dArt/";
             } else if (selectedObject === sphereMesh4) {
                 window.location.href = "https://www.instagram.com/hennohail/?hl=en";
-            } else if (selectedObject === sphereMesh5) {
-                window.location.href = "https://ganyuhail.github.io/enate/";
+            //} else if (selectedObject === sphereMesh5) {
+            //    window.location.href = "https://ganyuhail.github.io/enate/";
             } else if (selectedObject === sphereMesh6) {
                 window.location.href = "https://ganyuhail.github.io/mesmo1/";
             } else if (selectedObject === sphereMesh7) {
